58d1231eafe4e52f5b870158754e5509d2715a59
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-06-16 22:43:14 +0300

    .

--- components/ImageGrid.js
+++ components/ImageGrid.js
@@ -14,12 +14,12 @@ export const ResultGrid = ({ records, onOpenRecord, width = 500, height = 500 })
         const playable = extractVideoUrls(rec).length > 0;
         return (
           <Link key={rec.id} href={`/view?id=${encodeURIComponent(rec.id)}`}>
-            <li className={clsx("w-full sm:w-1/2 lg:w-1/3 mb-4 flex flex-col rounded-xl cursor-pointer group")} style={{  }}>
+            <li className={clsx("w-1/2 md:w-1/3 mb-4 flex flex-col rounded-xl cursor-pointer group")} style={{  }}>
               <div className="flex flex-col h-full">
                   <div className="flex items-center h-full p-3">
                     { rec.images &&
-                      <div className="flex items-center justify-center bg-gray-900 overflow-hidden w-full" style={{ minHeight: '300px' }}>
-                        <LazyLoad height={300} offset={100}>
+                      <div className="flex items-center justify-center bg-gray-900 overflow-hidden w-full" style={{ minHeight: '100px' }}>
+                        <LazyLoad height={200} offset={100}>
                           <img src={`https://api.finna.fi${rec.images[0]}&w=400`} width="300"
                              style={{
                                  width: '100%',
@@ -28,7 +28,7 @@ export const ResultGrid = ({ records, onOpenRecord, width = 500, height = 500 })
                         </LazyLoad>
                       </div> }
                   </div>
-                <div className="w-full bg-gray-900 px-2 text-center text-gray-200 overflow-hidden text-xl line-clamp-2 group-hover:text-white">{rec.title}</div>
+                <div className="w-full bg-gray-900 px-2 text-center text-gray-200 overflow-hidden text-sm md:text-md line-clamp-2 group-hover:text-white">{rec.title}</div>
               </div>
             </li>
           </Link>
