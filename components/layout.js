b1352f2820d06eda81212bcdcd1f1fc5dc801cd3
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-06-20 12:53:20 +0300

    .

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="bg-gray-900 px-5 pt-2 flex flex-col md:flex-row justify-betweeyn bg-gray-900 ">
         <div className="flex flex-wrap items-end align-center text-pink-500 stroke-current text-white">
           <Link href="/">
-            <div className="text-3xl bg-gradient-to-t from-pink-500 to-red-500 text-transparent bg-clip-text md:text-2xl mr-2 font-bold whitespace-nowrap hover:to-pink-500 cursor-pointer">Elävät kuvat.</div>
+            <div className="text-4xl bg-gradient-to-t from-red-500 to-pink-500 text-transparent bg-clip-text sm:text-5xl mr-2 font-bold hover:to-pink-500 cursor-pointer">Elävät kuvat.</div>
           </Link>
           <div className="flex items-end text-md font-serif text-gray-100 italic whitespace-nowrap -mt-1">suomalaisia lyhytelokuvia</div>
         </div>
