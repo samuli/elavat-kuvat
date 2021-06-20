96e9f7f586b7f79b0ec8fa0384cd47e36e477ce9
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-06-20 12:40:34 +0300

    .

--- components/layout.js
+++ components/layout.js
@@ -5,8 +5,8 @@ import { FaPlayCircle } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <header>
-      <div className="bg-gray-900 px-5 pt-2 flex flex-col md:flex-row justify-between bg-gray-900 ">
+    <>
+      <div className="bg-gray-900 px-5 pt-2 flex flex-col md:flex-row justify-betweeyn bg-gray-900 ">
         <div className="flex flex-wrap items-end align-center text-pink-500 stroke-current text-white">
           <Link href="/">
             <div className="text-3xl bg-gradient-to-t from-pink-500 to-red-500 text-transparent bg-clip-text md:text-2xl mr-2 font-bold whitespace-nowrap hover:to-pink-500 cursor-pointer">Elävät kuvat.</div>
@@ -17,13 +17,13 @@ const Header = () => {
       <div className="z-10 sticky top-0 shadow-lg">
         <Autocomplete />
       </div>
-    </header>
+    </>
   );
 };
 
 const Layout = ({ children }) => (
 
-  <div style={{ }} className="flex flex-col bg-gray-900 text-white align-center items-center overflow-x-hidden min-h-screen">
+  <div style={{ }} className="flex flex-col bg-gray-900 text-white align-center items-center min-h-screen">
     <div className="w-full max-w-6xl mb-10 flex flex-col flex-start flex-inline" style={{ }}>
       <Header />
       <main>{children}</main>
