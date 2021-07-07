7d00dc119d6459acc9b20cbf889ff160fa8656bb
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-07-07 17:26:02 +0300

    download record data.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
+const path = require('path');
+
 module.exports = {
   experimental: {
     optimizeCss: true,
+  },
+
+  webpack: (config, { webpack, buildId, isServer }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.CONFIG_BUILD_ID': JSON.stringify(buildId)
+      })
+    );
+    if (isServer) {
+      return {
+        ...config,
+        entry() {
+          return config.entry().then((entry) => ({
+            ...entry,
+            download: path.resolve(process.cwd(), 'lib/download.js')
+          }));
+        }
+      };
+    }
+
+    return config
   },
 };
