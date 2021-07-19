046e62b8e7602f76526f54063fc4982f16b22e45
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-07-19 09:21:39 +0300

    sitemap url encoding

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,15 @@
+const encodeFacet = value => value.replace(/\s\&\s/g, ' %26 ');
+
 module.exports = {
   siteUrl: 'https://www.elavatkuvat.fi',
-  generateRobotsTxt: true
-}
+  generateRobotsTxt: true,
+  transform: async (config, path) => {
+    return {
+      loc: encodeFacet(path),
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  }
+};
