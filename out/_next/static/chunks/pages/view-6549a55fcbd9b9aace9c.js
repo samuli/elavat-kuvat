904fac915098acc734dadcff5d771defb855d50f
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-07-20 17:39:20 +0300

    .

--- out/_next/static/chunks/pages/view-6549a55fcbd9b9aace9c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[891],{2866:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/view",function(){return u(8967)}])}},function(n){n.O(0,[216,457,967,774,888,179],(function(){return _=2866,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
