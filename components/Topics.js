bb20ab00efd45260cff4c29fc59606992022e803
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-07-20 17:47:48 +0300

    .

--- components/Topics.js
+++ components/Topics.js
@@ -6,13 +6,13 @@ export const FacetStripe = ( { facet, facets, facetUrl, truncate = false } ) => 
 <div className={clsx("mt-1 items-center w-full", truncate && `line-clamp-3 overflow-clip`)}>
   <ul>
     { facets.map(f => (
-      <li className="inline-flex flex-auto mr-2 mb-1 px-2 py-1 bg-gradient-to-b from-gray-100 to-gray-200 rounded-md text-xs font-medium subpixel-antialiased text-gray-800 uppercase. cursor-pointer ripple-bg-white whitespace-nowrap" key={`facet-${f.value}`}>
-        <AppLink prefetch={true} key={`facet-map-${f.value}`} href={facetUrl(facet, f.value)}>
-          <a>
-            {f.translated}
-          </a>
-        </AppLink>
-      </li>
+      <AppLink prefetch={true} key={`facet-map-${f.value}`} href={facetUrl(facet, f.value)}>
+        <a>
+          <li className="inline-flex flex-auto mr-2 mb-1 px-2 py-1 bg-gradient-to-b from-gray-100 to-gray-200 rounded-md text-xs font-medium subpixel-antialiased text-gray-800 uppercase. cursor-pointer ripple-bg-white whitespace-nowrap" key={`facet-${f.value}`}>
+                {f.translated}
+          </li>
+        </a>
+      </AppLink>
     )) }
   </ul>
   </div>
